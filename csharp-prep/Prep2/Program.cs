cdc1287a09fe647a5a351b1c065595ea59b8356f
Alexander Conte <alexanderconte@byui.edu>
2026-04-25 20:33:27 -0600

    Prep 2 complete.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int gradePercent = int.Parse(userAnswer);
+
+        string letterGrade = "";
+
+        if (gradePercent >= 93)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 90)
+        {
+            letterGrade = "A-";
+        }
+        else if (gradePercent >= 87)
+        {
+            letterGrade = "B+";
+        }
+        else if (gradePercent >= 83)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B-";
+        }
+        else if (gradePercent >= 77)
+        {
+            letterGrade = "C+";
+        }
+        else if (gradePercent >= 73)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercent >= 70)
+        {
+            letterGrade = "C-";
+        }
+        else if (gradePercent >= 67)
+        {
+            letterGrade = "D+";
+        }
+        else if (gradePercent >= 63)
+        {
+            letterGrade = "D";
+        }
+        else if (gradePercent >= 60)
+        {
+            letterGrade = "D-";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("A WINNER IS YOU");
+        }
+        else
+        {
+            Console.WriteLine("Come back when you can put up a fight.");
+        }
     }
 }
