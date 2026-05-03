32883fbd38caf6e1728f421b99851ff2a21eb0a8
3334PDA <3334privatedriveaccess@gmail.com>
2026-05-02 23:17:28 -0600

    Prep 4 complete.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new();
+
+        int userNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
+        int min = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < min && number > 0)
+            {
+                min = number;
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {min}");
     }
 }
