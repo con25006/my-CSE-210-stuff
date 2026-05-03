dff52553ac95b55c1857ffa0fb0d8a615d01c7b2
3334PDA <3334privatedriveaccess@gmail.com>
2026-05-02 23:51:50 -0600

    Prep 5 complete.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        ProgramStartMessage();
+
+        string userName = AskName();
+
+        int favoriteNumber = AskFavNumber();
+
+        int squaredNumber = SquareGivenNumber(favoriteNumber);
+
+        int birthYear;
+
+        AskBirthYear(out birthYear);
+
+        FinalResult(userName, squaredNumber, birthYear);
+    }
+
+    static void ProgramStartMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string AskName()
+    {
+        Console.Write("Please enter your name: ");
+
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int AskFavNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    
+    static void AskBirthYear(out int birthYear)
+    {
+        Console.Write($"Please enter the year you were born: ");
+
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareGivenNumber(int number)
+    {
+        int square = number * number;
+        
+        return square;
+    }
+
+    static void FinalResult(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2026 - birthYear} years old this year.");
     }
 }
