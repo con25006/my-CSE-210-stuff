873f3a67a100eea0704e82d26b4b85ddbe219347
3334PDA <3334privatedriveaccess@gmail.com>
2026-05-02 21:23:53 -0600

    Prep 3 complete.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int numberKnown = randomGenerator.Next(1, 101);
+
+        int userGuess = -1;
+
+        while (userGuess != numberKnown)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (numberKnown > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numberKnown < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
